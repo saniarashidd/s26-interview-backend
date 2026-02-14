1068fa5f2986f71b591d2a408b08a89e275beaee
saniarashidd <sania.rashid928@gmail.com>
2026-02-14 13:08:41 -0800

    change url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-export const dynamic = "force-dynamic";
+import { createClient } from "@supabase/supabase-js";
 
 export default async function Home() {
-  const res = await fetch("/api/projects");
-  const json = await res.json();
-
-  return (
-    <pre>{JSON.stringify(json, null, 2)}</pre>
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_HIBISCUS_SUPABASE_API_URL!,
+    process.env.NEXT_PUBLIC_HIBISCUS_SUPABASE_ANON_KEY!
   );
+
+  const { data, error } = await supabase
+    .from("projects")
+    .select("*")
+    .order("rank");
+
+  if (error) {
+    return <pre>{error.message}</pre>;
+  }
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
