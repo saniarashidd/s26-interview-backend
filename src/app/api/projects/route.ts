177ca6a995bfd4b1a07e29d64f13aed1ddc9aa60
saniarashidd <sania.rashid928@gmail.com>
2026-02-14 13:58:50 -0800

    CORS

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,25 +1,63 @@
+// import { createClient } from "@supabase/supabase-js";
+// import { unstable_noStore } from "next/cache";
+
+// export async function GET() {
+//   const supabase = createClient(
+//     process.env.NEXT_PUBLIC_HIBISCUS_SUPABASE_API_URL!,
+//     process.env.NEXT_PUBLIC_HIBISCUS_SUPABASE_ANON_KEY!
+//   );
+//   unstable_noStore();
+
+//   const res = await supabase.from("projects").select("*");
+
+//   if (res.error != null) {
+//     const error = `${res.error.message}: ${res.error.details}`;
+//     return Response.json(
+//       { error },
+//       { status: res.status, statusText: res.statusText }
+//     );
+//   }
+
+//   return Response.json(
+//     { data: res.data.toSorted((a, b) => a.rank - b.rank) },
+//     { status: 200 }
+//   );
+// }
+
 import { createClient } from "@supabase/supabase-js";
 import { unstable_noStore } from "next/cache";
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return new Response(null, { headers });
+}
+
 export async function GET() {
+  unstable_noStore();
+
   const supabase = createClient(
     process.env.NEXT_PUBLIC_HIBISCUS_SUPABASE_API_URL!,
     process.env.NEXT_PUBLIC_HIBISCUS_SUPABASE_ANON_KEY!
   );
-  unstable_noStore();
 
   const res = await supabase.from("projects").select("*");
 
-  if (res.error != null) {
-    const error = `${res.error.message}: ${res.error.details}`;
-    return Response.json(
-      { error },
-      { status: res.status, statusText: res.statusText }
+  if (res.error) {
+    return new Response(
+      JSON.stringify({ error: res.error.message }),
+      { status: 500, headers }
     );
   }
 
-  return Response.json(
-    { data: res.data.toSorted((a, b) => a.rank - b.rank) },
-    { status: 200 }
+  return new Response(
+    JSON.stringify({
+      data: res.data.toSorted((a, b) => a.rank - b.rank),
+    }),
+    { status: 200, headers }
   );
 }
