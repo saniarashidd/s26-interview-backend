5291b8c461efd49ea4cd09a773dcbe307372362a
saniarashidd <sania.rashid928@gmail.com>
2026-02-14 12:09:58 -0800

    removed localhost url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-export default async function Home() {
-  // const data = await fetch(
-  //   "http://localhost:3000/api/rank/0c77ad6b-5cfb-425e-a827-1d695a003099",
-  //   { method: "PUT", body: JSON.stringify({ rank: 3 }) }
-  // );
+export const dynamic = "force-dynamic";
 
-  await fetch("http://localhost:3000/api/reset", { method: "POST" });
-  const data = await fetch("http://localhost:3000/api/projects");
-  const json = await data.json();
-  return <div>{JSON.stringify(json)}</div>;
+export default async function Home() {
+  const res = await fetch("/api/projects");
+  const json = await res.json();
+
+  return (
+    <pre>{JSON.stringify(json, null, 2)}</pre>
+  );
 }
